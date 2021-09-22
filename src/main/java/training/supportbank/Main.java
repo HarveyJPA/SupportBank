a436875c63925ada4ce6a655dfed1e715ff61510
HarveyJPA <Harvey.J.ParkerAcheson@aexp.com>
2021-09-22 09:49:35 +0100

    Implemented methods to import and then read and convert a .csv file into a list

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,8 +1,38 @@
 package training.supportbank;
 
+import com.opencsv.CSVReader;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
     public static void main(String args[]) {
-        // Your code here!
-        System.out.println("Test!");
+        try {
+            System.out.print(importCSV());
+        } catch (Exception  e) {
+            e.printStackTrace();
+        }
     }
+
+    private static List<String[]> readCSV(Reader reader) throws Exception {
+        List<String[]> list = new ArrayList<>();
+        CSVReader csvReader = new CSVReader(reader);
+        String[] line;
+        while ((line = csvReader.readNext()) != null) {
+            list.add(line);
+        }
+        reader.close();
+        csvReader.close();
+        return list;
+    }
+
+    private static String importCSV() throws Exception {
+        String filePath = "Transactions2014.csv";
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+        return readCSV(bufferedReader).toString();
+    }
+
 }
